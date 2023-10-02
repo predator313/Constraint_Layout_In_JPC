78a42361b8dd436be65e032e955102babd7bd784
Android Dev <aamirnasa97@gmail.com>
2023-10-02 23:44:54 +0530

    constraint layout

--- app/src/main/java/com/aamirashraf/constraint_layout_in_jpc/MainActivity.kt
+++ app/src/main/java/com/aamirashraf/constraint_layout_in_jpc/MainActivity.kt
@@ -3,20 +3,61 @@ package com.aamirashraf.constraint_layout_in_jpc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.HorizontalChainReference
 import com.aamirashraf.constraint_layout_in_jpc.ui.theme.Constraint_Layout_in_JPCTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val constraintSet= ConstraintSet {
+                val greenBox=createRefFor("greenBox")
+                val redBox=createRefFor("redBox")
+                constrain(greenBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width= Dimension.value(100.dp)
+                    height=Dimension.value(100.dp)
+                }
 
+                constrain(redBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    width=Dimension.value(100.dp)
+                    height=Dimension.value(100.dp)
+
+                }
+                createHorizontalChain(greenBox,redBox)
+            }
+            ConstraintLayout(constraintSet, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier
+                    .background(Color.Green)
+                    .layoutId("greenBox")
+
+                ) {
+
+                }
+                Box(modifier = Modifier.background(Color.Red)
+                    .layoutId("redBox"))
+            }
         }
     }
 }
